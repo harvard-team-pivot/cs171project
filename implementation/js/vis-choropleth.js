68bcdcc6fe03e058a9dcb4fca98800ea45b468ef
Sean Keery <skibum55@hotmail.com>
2016-04-25 16:08:20 -0400

    oridomi

--- implementation/js/vis-choropleth.js
+++ implementation/js/vis-choropleth.js
@@ -52,6 +52,8 @@ var quantize = d3.scale.quantize()
 
 var malariaDataByCountryId = [];
 
+var folded;
+
 // Initialize tooltip
 var tip = d3.tip().attr('class', 'd3-tip').html(function (d) {
     return "The " + d.Country + " is " + d.CustomsScore + " feet tall.";
@@ -201,6 +203,17 @@ function updateChoropleth(dataYears2014, world) {
                 }
             }
         });
+
+    folded = new OriDomi('#chart-area', {
+        vPanels:         5,     // number of panels when folding left or right (vertically oriented)
+        hPanels:         3,     // number of panels when folding top or bottom
+        speed:           1200,  // folding duration in ms
+        ripple:          2,     // backwards ripple effect when animating
+        shadingIntesity: .5,    // lessen the shading effect
+        perspective:     800,   // smaller values exaggerate 3D distortion
+        maxAngle:        40,    // keep the user's folds within a range of -40 to 40 degrees
+        shading:         'soft' // change the shading type
+    });
 
 }
 
